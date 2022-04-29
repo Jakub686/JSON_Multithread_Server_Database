6c67d39a50d3ccada6bb98421d684287c017ce0d
Jakub <jakub.bchyla@gmail.com>
2022-04-29 14:52:51 +0200

    Initial commit

---
+++ JSON Database/task/src/client/Main.java
@@ -0,0 +1,20 @@
+package client;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
+
+class Model{
+
+}
+
+class View{
+
+}
+
+class Controller{
+
+}
