4bfbfedb544a7b8ab8d108e40d3346c57f02db24
Jakub <jakub.bchyla@gmail.com>
2022-05-02 15:35:03 +0200

    a

--- JSON Database/task/src/client/Main.java
+++ JSON Database/task/src/client/Main.java
@@ -1,11 +1,33 @@
 package client;
 
-import server.Controller;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static final String SERVER_ADDRESS = "127.0.0.1";
+    private static final int SERVER_PORT = 34522;
 
+    public static void main(String[] args) {
+        System.out.println("Client started");
+        try (
+                Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+                DataInputStream input = new DataInputStream(socket.getInputStream());
+                DataOutputStream output = new DataOutputStream(socket.getOutputStream())
+        ) {
+            Scanner scanner = new Scanner(System.in);
+            String msg = scanner.nextLine();
+
+            output.writeUTF(msg); // sending message to the server
+            System.out.println("Client sent "+ msg);
+            String receivedMsg = input.readUTF(); // response message
+
+            System.out.println("Received from server: " + receivedMsg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
