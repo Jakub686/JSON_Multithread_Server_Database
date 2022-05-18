0ce77910a27aae72fd47b913bfa9f8b0f444c140
Jakub <jakub.bchyla@gmail.com>
2022-05-18 15:47:48 +0200

    last test

--- JSON Database/task/src/client/Main.java
+++ JSON Database/task/src/client/Main.java
@@ -67,7 +67,6 @@ public class Main {
 
             System.out.println("Received: " + jsonRespond);
             //Deserialization
-
         } catch (IOException e) {
             e.printStackTrace();
         }
